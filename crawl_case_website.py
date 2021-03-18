2c368ee3026afc497a720bbcdccca89d91f26fcf
pei-jan <70442171+pei-jan@users.noreply.github.com>
2021-03-18 13:54:19 +0800

    Update crawl_case_website.py

--- crawl_case_website.py
+++ crawl_case_website.py
@@ -13,7 +13,7 @@ import streamlit as st
 import base64
 from io import BytesIO
 st.title('招標網團險案件查詢')
-st.markdown(f'<p><span style = "font-size:24px"><strong>即時搜尋政府招標網，篩選團險案件</strong></span></p>',unsafe_allow_html=True)
+st.markdown(f'<p><span style = "font-size:20px"><strong>即時搜尋政府招標網，篩選團險案件</strong></span></p>',unsafe_allow_html=True)
 requests.packages.urllib3.disable_warnings()
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
@@ -29,20 +29,20 @@ remain_days = []
 website = []
 
 關鍵字 = st.text_input("搜尋關鍵字(預設為：險)",value='險')
-st.markdown(f'<p><span style = "font-size:24px"><strong>篩選關鍵字具其一則顯示，可自行修改</strong></span></p>',unsafe_allow_html=True)
-st.markdown(f'<p><span style = "font-size:24px"><strong>如欲搜尋防汛，可將其中一項條件改為防汛，不需之條件輸入X)</strong></span></p>',unsafe_allow_html=True)
-關鍵字2_1 = st.text_input("篩選關鍵字包含(1)",value='團體')
-關鍵字2_2 = st.text_input("篩選關鍵字包含(2)",value='鄉民')
-關鍵字2_3 = st.text_input("篩選關鍵字包含(3)",value='區民')
-關鍵字2_4 = st.text_input("篩選關鍵字包含(4)",value='鎮民')
-關鍵字2_5 = st.text_input("篩選關鍵字包含(5)",value='市民')
-關鍵字2_6 = st.text_input("篩選關鍵字包含(6)",value='義勇')
-關鍵字2_7 = st.text_input("篩選關鍵字包含(7)",value='義消')
-關鍵字2_8 = st.text_input("篩選關鍵字包含(8)",value='志工')
-關鍵字2_9 = st.text_input("篩選關鍵字包含(9)",value='守望相助')
-st.markdown(f'<p><span style = "font-size:24px"><strong>篩選關鍵字不含條件，具其一則不顯示</strong></span></p>',unsafe_allow_html=True)
-關鍵字2_10 = st.text_input("篩選關鍵字不包含(1)",value='開口合約')
-關鍵字2_11 = st.text_input("篩選關鍵字不包含(2)",value='責任')
+st.markdown(f'<p><span style = "font-size:20px"><strong>篩選關鍵字包含其一則顯示，預設為團險件關鍵字，可自行修改</strong></span></p>',unsafe_allow_html=True)
+st.markdown(f'<p><span style = "font-size:20px"><strong>如欲搜尋防汛，可將其中一項條件改為防汛，不需之條件輸入X)</strong></span></p>',unsafe_allow_html=True)
+關鍵字2_1 = st.text_input("篩選關鍵字(1)",value='團體')
+關鍵字2_2 = st.text_input("篩選關鍵字(2)",value='鄉民')
+關鍵字2_3 = st.text_input("篩選關鍵字(3)",value='區民')
+關鍵字2_4 = st.text_input("篩選關鍵字(4)",value='鎮民')
+關鍵字2_5 = st.text_input("篩選關鍵字(5)",value='市民')
+關鍵字2_6 = st.text_input("篩選關鍵字(6)",value='義勇')
+關鍵字2_7 = st.text_input("篩選關鍵字(7)",value='義消')
+關鍵字2_8 = st.text_input("篩選關鍵字(8)",value='志工')
+關鍵字2_9 = st.text_input("篩選關鍵字(9)",value='守望相助')
+st.markdown(f'<p><span style = "font-size:20px"><strong>篩選關鍵字不含條件，具其一則不顯示，可自行修改</strong></span></p>',unsafe_allow_html=True)
+關鍵字2_10 = st.text_input("篩選關鍵字不含(1)",value='開口合約')
+關鍵字2_11 = st.text_input("篩選關鍵字不含(2)",value='責任')
 
 
 
