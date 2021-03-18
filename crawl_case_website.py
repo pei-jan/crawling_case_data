86a5828a74db8b15e7f5b65342c791095ec284b3
pei-jan <70442171+pei-jan@users.noreply.github.com>
2021-03-18 12:29:37 +0800

    Update crawl_case_website.py

--- crawl_case_website.py
+++ crawl_case_website.py
@@ -215,6 +215,7 @@ if start:
 try:
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
     df2 = df.drop('標案網址',axis=1)
+    st.markdown(get_table_download_link(df))
     st.table(df2)
     
 except:
